f6d326bc8dbec893489a6ae8ba4763c727bd37de
marcelochirai <65429412+marcelochirai@users.noreply.github.com>
2020-09-23 23:17:24 -0300

    Add files via upload
    2) FAZER UM PROGRAMA PARA LER NOME E SALÁRIO DE DOIS FUNCIONÁRIOS, E QUE CALCULE A MÉDIA SALARIAL.

---
+++ Pessoa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.CompilerServices;
+
+namespace MediaSalario
+{
+    class Pessoa
+    {
+        public string Nome;
+        public double Salario;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+
+namespace MediaSalario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1, p2;  //Declaração das variáveis.
+
+            p1 = new Pessoa();   //Atribuição de classe.
+            p2 = new Pessoa();
+
+            Console.Write("Informe o nome da primeira pessoa: ");
+            p1.Nome = Console.ReadLine();
+            Console.Write("Informe o salário: ");
+            p1.Salario = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.Write("Informe o nome da segunda pessoa: ");
+            p2.Nome = Console.ReadLine();
+            Console.Write("Informe o salário: ");
+            p2.Salario = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.WriteLine("A média dos salários é: " + ((p1.Salario + p2.Salario) / 2).ToString("F2",CultureInfo.InvariantCulture));
+
+            Console.ReadKey();
+        }
+    }
+}
